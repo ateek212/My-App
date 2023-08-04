af3ea0b47961651db6cd01606efd681a0a13e05b
ateek212 <ateek3060@gmail.com>
2023-08-04 18:30:48 +0530

    added the folder structures

--- My-App/src/app/app.module.ts
+++ My-App/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { HeaderbarComponent } from './layout/headerbar/headerbar.component';
+import { FooterbarComponent } from './layout/footerbar/footerbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderbarComponent,
+    FooterbarComponent
   ],
   imports: [
     BrowserModule,
